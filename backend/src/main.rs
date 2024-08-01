903f2591644737e782ee9dbe27712d677b46a947
henrikvtcodes <commits@henrikvt.com>
2024-07-31 19:09:32 -0500

    init backend with axum

---
+++ backend/src/main.rs
@@ -0,0 +1,16 @@
+use axum::{routing::get, Router};
+mod api;
+use api:: get_bgp;
+
+get_bgp();
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new();
+    app.route("/bgp", get(|| async { "Hello, BGP!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
