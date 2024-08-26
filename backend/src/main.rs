fd1b70dfe1122532fb9cad94e9fa47cc00eaad17
henrikvtcodes <commits@henrikvt.com>
2024-08-26 11:09:01 -0400

    working docker image (primarily for testing)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,55 @@
-use axum::{routing::get, Router};
+use api::{make_bgp_router, make_traceroute_router};
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::Deserialize;
 mod api;
-use api:: get_bgp;
+mod mtr;
+use std::net::IpAddr;
 
-get_bgp();
+#[derive(Debug, Deserialize)]
+struct PingRequest {
+    ip: IpAddr,
+}
+
+async fn ping_handler(Json(input): Json<PingRequest>) -> impl IntoResponse {
+    let ip_str = input.ip.to_string();
+    if input.ip.is_ipv4() {
+        println!("Pinging IPv4 address: {}", ip_str);
+        (StatusCode::OK, Json("{ message: 'IPv4 Address Accepted' }"))
+    } else if input.ip.is_ipv6() {
+        println!("Pinging IPv6 address: {}", ip_str);
+        (StatusCode::OK, Json("{ message: 'IPv6 Address Accepted' }"))
+    } else {
+        println!("Invalid IP address");
+        (
+            StatusCode::BAD_REQUEST,
+            Json("{ message: 'Invalid IP address' }"),
+        )
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new();
-    app.route("/bgp", get(|| async { "Hello, BGP!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Alive!" }))
+        // .merge(make_ping_router())
+        .route("/ping", post(ping_handler))
+        .merge(make_bgp_router())
+        .merge(make_traceroute_router())    // .route("/test", post(post_handle_test))
+    ;
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // run our app with hyper, listening globally on port 2152
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:2152").await.unwrap();
+    println!("Server running on port 2152");
     axum::serve(listener, app).await.unwrap();
 }
+
+// async fn post_handle_test(r: Request<Body>) -> impl IntoResponse {
+//     let body = r.body();
+
+//     StatusCode::BAD_REQUEST
+// }
