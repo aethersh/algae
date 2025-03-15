59106e3eac217f4ed9fa07eb9bc7e474cfb2c262
Henrik VT <commits@henrikvt.com>
2025-03-14 23:23:59 -0400

    add nix flake devshell

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "AetherNet Looking Glass";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, rust-overlay, ... }:
+    let
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ (import rust-overlay) ];
+            };
+            inherit system;
+          }
+        );
+    in
+
+    {
+
+      devShells = forEachSupportedSystem (
+        { pkgs, ... }:
+        {
+          default = pkgs.mkShell rec {
+            buildInputs = with pkgs; [
+              openssl
+              protobuf
+            ];
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              (rust-bin.selectLatestNightlyWith (
+                toolchain: toolchain.default.override { extensions = [ "rust-src" ]; }
+              ))
+              rust-analyzer
+            ];
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          };
+        }
+      );
+    };
+}
