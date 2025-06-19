a00e7ef949f54457d52c687af402923bc6d829dc
Henrik VT <commits@henrikvt.com>
2025-06-19 12:41:42 -0400

    http stuff

--- algae.go
+++ algae.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"embed"
+	"net/http"
+
 	"github.com/aethersh/algae/templates"
 	"github.com/aethersh/algae/util"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
 )
+
+// Embed a directory
+//go:embed static/*
+var embedDirStatic embed.FS
 
 func main() {
 	app := fiber.New()
 
+
+	// CORS Configuration
+	corsConfig, err := util.GenerateCORSConfig()
+	if err != nil {
+		log.Fatalf("Failed to generate CORS config: %v", err)
+	}
+	app.Use(cors.New(*corsConfig))
+
+	// Static File Serving
+	app.Use("/static", filesystem.New(filesystem.Config{
+		Root: http.FS(embedDirStatic),
+		PathPrefix: "static",
+		Browse: false,
+	}))
+
+	// Routes
 	app.Get("/:name?", func(c *fiber.Ctx) error {
 		name := c.Params("name")
 		c.Locals("name", name)
---
+++ util/constants.go
@@ -0,0 +1,3 @@
+package util
+
+const ENVCONFIG_PREFIX = "ALGAE"
