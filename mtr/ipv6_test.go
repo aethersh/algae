5e0782d5fb9b93cf2d1a2334a3aad47dfd60c03a
Henrik VT <commits@henrikvt.com>
2025-06-22 23:50:56 -0400

    format

--- mtr/ipv6_test.go
+++ mtr/ipv6_test.go
@@ -47,11 +47,11 @@ func TestValidateIPv6Host(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.host, func(t *testing.T) {
 			addr, host, err := mtr.ValidateIPv6Host(tt.host)
-			
+
 			if tt.valid {
 				assert.NoError(t, err)
 				assert.NotNil(t, addr)
-				
+
 				hstStr := *host
 				assert.Equal(t, tt.host, hstStr)
 			} else {
